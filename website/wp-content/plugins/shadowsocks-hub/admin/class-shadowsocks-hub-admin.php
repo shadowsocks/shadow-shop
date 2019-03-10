ba51323a437d2a51eef0868c485bc0056aa4ae5d
Eggham <eggham.carnegie@gmail.com>
2019-03-10 18:14:04 +0800

    add simple custom rest api

--- website/wp-content/plugins/shadowsocks-hub/admin/class-shadowsocks-hub-admin.php
+++ website/wp-content/plugins/shadowsocks-hub/admin/class-shadowsocks-hub-admin.php
@@ -399,6 +399,29 @@ class Shadowsocks_Hub_Admin
 	}
 
 	/**
+ * This is our callback function that embeds our phrase in a WP_REST_Response
+ */
+function prefix_get_endpoint_phrase() {
+	error_log("reached prefix_get_endpoint_phrase");
+    // rest_ensure_response() wraps the data we want to return into a WP_REST_Response, and ensures it will be properly returned.
+    return rest_ensure_response( 'Hello World, this is the WordPress REST API' );
+}
+
+	/**
+ * This function is where we register our routes for our example endpoint.
+ */
+public function prefix_register_example_routes() {
+	error_log("reached prefix_register_example_routes");
+    // register_rest_route() handles more arguments but we are going to stick to the basics for now.
+    register_rest_route( 'hello-world/v1', '/phrase', array(
+        // By using this constant we ensure that when the WP_REST_Server changes our readable endpoints will work as intended.
+        'methods'  => WP_REST_Server::READABLE,
+        // Here we register our callback. The callback is fired when this endpoint is matched by the WP_REST_Server class.
+        'callback' => array($this, 'prefix_get_endpoint_phrase'),
+    ) );
+}
+
+	/**
 	 * Register the stylesheets for the admin area.
 	 *
 	 * @since    1.0.0
